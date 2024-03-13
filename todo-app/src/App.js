9a2d98175ed3a90a2a48f1656d76eea527af4288
hy43 <hyde0401@gmail.com>
2024-03-13 21:12:55 +0900

    style : checkbox and button

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,40 @@
 import React, {Component } from "react";
+import "./App.css";
 
 export default class App extends Component {
+
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      TextDecoration: "none"
+    }
+  }
+
   render() {
-    return (<div>hello</div>);
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title ">
+            <h1>할일 목록</h1>            
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} />
+            세수하기
+            <button style={this.btnStyle}>x</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
